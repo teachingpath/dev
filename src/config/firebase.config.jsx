fbd910fcf743e5504e8dfa3bdcb562c898a15f8d
Raul A. Alzate <raul.alzate@sofka.com.co>
2021-03-31 16:46:06 -0500

    Init commit

---
+++ src/config/firebase.config.jsx
@@ -0,0 +1,15 @@
+/*
+ * Firebase Configuration
+ * Get the configuration in your firebase console page
+ */
+
+const FIREBASE = {
+  apiKey: "AIzaSyCTEUvOS5cYl_kj_-4HJweAgniYhR_clhM",
+  authDomain: "teachingpath-co.firebaseapp.com",
+  projectId: "teachingpath-co",
+  storageBucket: "teachingpath-co.appspot.com",
+  messagingSenderId: "654834840574",
+  appId: "1:654834840574:web:0bdd86943eaeb1f7af06af"
+}
+
+export default FIREBASE
